1ad86be69d873e5e61b3ecc1c8aaf779ef11ed2e
EvyOliveira <evelynoliveira.eo@gmail.com>
2024-07-13 17:35:56 -0300

    Add order struct and validation

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/EvyOliveira/product-fee
+
+go 1.22.5
---
+++ internal/order/entity/order.go
@@ -0,0 +1,23 @@
+package entity
+
+import "errors"
+
+type Order struct {
+	ID         string
+	Price      float64
+	Tax        float64
+	FinalPrice float64
+}
+
+func (o *Order) IsValid() error {
+	if o.ID == "" {
+		return errors.New("invalid id")
+	}
+	if o.Price == 0 {
+		return errors.New("invalid price")
+	}
+	if o.Tax == 0 {
+		return errors.New("invalid tax")
+	}
+	return nil
+}
