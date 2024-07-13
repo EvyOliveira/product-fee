45eac19c960e7ef6131a92422efaf0e7b5f137b3
EvyOliveira <evelynoliveira.eo@gmail.com>
2024-07-13 18:09:44 -0300

    Add order repository and order repository interface

---
+++ cmd/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/EvyOliveira/product-fee/internal/order/entity"
+)
+
+func main() {
+	order, err := entity.NewOrder("123", 10, 2)
+	if err != nil {
+		panic(err)
+	}
+	err = order.CalculateFinalPrice()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("The final price is: %f\n", order.FinalPrice)
+}
