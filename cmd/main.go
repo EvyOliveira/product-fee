2ed88917ccb99adee9d9934accf74f7434af91b3
EvyOliveira <evelynoliveira.eo@gmail.com>
2024-07-14 23:14:10 -0300

    Refactor RabbitMQ error messages

--- cmd/main.go
+++ cmd/main.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
+	"sync"
+	"time"
 
 	"github.com/EvyOliveira/product-fee/internal/order/infra/database"
 	"github.com/EvyOliveira/product-fee/internal/order/usecase"
+	rabbitmq "github.com/EvyOliveira/product-fee/pkg/rabbitmq"
 	_ "github.com/go-sql-driver/mysql"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
+	maxWorkers := 3
+	wg := sync.WaitGroup{}
+
 	dbDriver := "mysql"
 	dbUser := "user"
 	dbPassword := "root"
@@ -25,15 +33,37 @@ func main() {
 
 	repository := database.NewOrderRepository(db)
 	uc := usecase.NewCalculateFinalPriceUseCase(repository)
-	input := usecase.OrderInputDTO{
-		ID:    "456",
-		Price: 100,
-		Tax:   10,
-	}
 
-	output, err := uc.Execute(input)
+	ch, err := rabbitmq.OpenChannel()
 	if err != nil {
-		log.Fatal("error to execute a new order:", err)
+		log.Fatal("unable to open a channel with rabbitmq: ", err)
 	}
-	fmt.Printf("the final price is: %f\n", output.FinalPrice)
+	defer ch.Close()
+	out := make(chan amqp.Delivery)
+	go rabbitmq.Consume(ch, out)
+
+	wg.Add(maxWorkers)
+	for i := 0; i < maxWorkers; i++ {
+		defer wg.Done()
+		go worker(out, uc, i)
+	}
+	wg.Wait()
+}
+
+func worker(deliveryMessage <-chan amqp.Delivery, uc *usecase.CalculateFinalPriceUseCase, workerId int) {
+	for msg := range deliveryMessage {
+		var input usecase.OrderInputDTO
+		err := json.Unmarshal(msg.Body, &input)
+		if err != nil {
+			fmt.Println("error unmarshalling message", err)
+		}
+		input.Tax = 10.0
+		_, err = uc.Execute(input)
+		if err != nil {
+			fmt.Println("error to execute order", err)
+		}
+		msg.Ack(false) //notifica o RabbitMQ que já processou a mensagem e que pode retirá-la da fila
+		fmt.Println("worker", workerId, "processed order", input.ID)
+		time.Sleep(1 * time.Second)
+	}
 }
